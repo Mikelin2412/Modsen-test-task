1182739600003427cc8f1e33bcbab3cc5929e895
Mikelin2412 <mr_davidov2412@mail.ru>
2024-06-23 13:40:34 +0300

    feat: add ErrorBoundary pattern

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,20 +1,15 @@
-import React, { createContext } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '@components/header';
 import Footer from '@components/footer';
 import { GlobalStyles, MainContent, fontStyles } from '@utils/global_styles';
 import { ThemeProvider } from 'styled-components';
-import useFetch from '@utils/hooks/useFetch';
-import { IArtworks } from '@utils/interfaces';
-
-export const ArtsProvider = createContext<IArtworks | null>(null);
+import ErrorBoundary from '@utils/errors/error_boundaries';
 
 const Layout: React.FunctionComponent = () => {
-  const { data } = useFetch<IArtworks>('https://api.artic.edu/api/v1/artworks');
-
   return (
     <>
-      <ArtsProvider.Provider value={data}>
+      <ErrorBoundary>
         <ThemeProvider theme={fontStyles}>
           <GlobalStyles />
           <Header />
@@ -23,7 +18,7 @@ const Layout: React.FunctionComponent = () => {
           </MainContent>
           <Footer />
         </ThemeProvider>
-      </ArtsProvider.Provider>
+      </ErrorBoundary>
     </>
   );
 };
